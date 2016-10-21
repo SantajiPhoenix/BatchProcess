b546abcd4cd20d48f68d17ee03445ff535b4a651
Bharat Tripathi <bharat.tripathi@yestelecom.nl>
2016-10-21 10:32:04 +0000

    Added loggers

--- src/main/java/nl/yestelecom/phoenix/batch/sender/mail/MailSenderUtil.java
+++ src/main/java/nl/yestelecom/phoenix/batch/sender/mail/MailSenderUtil.java
@@ -7,6 +7,8 @@ import java.util.StringTokenizer;
 
 import javax.mail.internet.MimeMessage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -17,17 +19,16 @@ import org.springframework.stereotype.Service;
 public class MailSenderUtil {
 	@Autowired
     private JavaMailSender mailSender;
+	
+	private static Logger logger = LoggerFactory.getLogger(MailSenderUtil.class);
 
 	public void sendMail(String emailTo, String emailFrom, String path, String subject, String text) throws Exception{
-		
- 		
  		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
  		MimeMessage message = mailSender.createMimeMessage();
  		simpleMailMessage.setFrom(emailFrom);
  		simpleMailMessage.setTo(getToList(emailTo));
  		simpleMailMessage.setSubject(subject);
  		simpleMailMessage.setText(text);
-		
 		MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
 		helper.setFrom(simpleMailMessage.getFrom());
@@ -41,23 +42,22 @@ public class MailSenderUtil {
 		File[] listOfFiles = folder.listFiles();
 		for (int i = 0; i < listOfFiles.length; i++) {
 		      if (listOfFiles[i].isFile()) {
-		        System.out.println("File " + listOfFiles[i].getName());
 		        helper.addAttachment(listOfFiles[i].getName(), listOfFiles[i]);
 		      } else if (listOfFiles[i].isDirectory()) {
-		        System.out.println("Directory " + listOfFiles[i].getName());
 		      }
 		   }
 		
 		mailSender.send(message);
+		logger.info("Mail sent");
  	
  	}
 	
 	private String[] getToList(String emailTo) {
+		logger.info("Getting to List");
 		List<String> toList = new ArrayList<String>();
 		StringTokenizer st = new StringTokenizer(emailTo,",");  
 		 while (st.hasMoreTokens()) {  
 	         String str = st.nextToken();
-	         System.out.println("To >> "+str);
 	         toList.add(str); 
 	     }  
 		 String[] toListArr = new String[toList.size()];
