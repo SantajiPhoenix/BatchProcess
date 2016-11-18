8db7ee2d985a7078543ea9beda2e9a423c27909a
Bharat Tripathi <bharat.tripathi@yestelecom.nl>
2016-11-18 10:23:46 +0000

    Changed model according to query

--- src/main/java/nl/yestelecom/phoenix/batch/job/marketpoints/model/MarketPoints.java
+++ src/main/java/nl/yestelecom/phoenix/batch/job/marketpoints/model/MarketPoints.java
@@ -8,19 +8,18 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "TEMP_VIEW_POINTS_PER_CONTRACT")
+@Table(name = "VIEW_POINTS_PER_CONTRACT")
 @Data
 public class MarketPoints {
-	@Column(name="VOICE_POINTS")
-	long voicePoints;
+	@Column(name="VOICE_POINTS",nullable=true)
+	Long voicePoints;
 	
-	@Column(name="DATA_POINTS")
-	long dataPoints;
+	@Column(name="DATA_POINTS",nullable=true)
+	Long dataPoints;
 	
 	@Column(name="PRODUCT_POINTS",nullable=true)
-	long productPoints;
+	Long productPoints;
 	
-
 	@Column(name="CODE")
 	String code;
 	
@@ -36,11 +35,8 @@ public class MarketPoints {
 	@Column(name="CONTRACT_NR")
 	String contractNr;
 	
-	@Column(name="INCENTIVE")
-	Long incentive;
-	
 	@Id
 	@Column(name="ID")
-	long id;
+	Long id;
 	
 }
--- src/main/java/nl/yestelecom/phoenix/batch/job/marketpoints/model/MarketPointsTotal.java
+++ src/main/java/nl/yestelecom/phoenix/batch/job/marketpoints/model/MarketPointsTotal.java
@@ -10,30 +10,29 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "TEMP_VIEW_POINTS_TOTAL")
+@Table(name = "VIEW_POINTS_TOTAL")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MarketPointsTotal {
 	
 	@Column(name="VOICE_POINTS")
-	long voicePoints;
+	Long voicePoints;
 	
 	@Column(name="DATA_POINTS")
-	long dataPoints;
+	Long dataPoints;
 	
 	@Column(name="PRODUCT_POINTS")
-	long productPoints;
+	Long productPoints;
 	
 	@Column(name="USED_POINTS")
-	long usedPoints;
+	Long usedPoints;
 	
 	@Column(name="TOTAL")
-	long total;
+	Long total;
 	
 	@Column(name="GRAND_TOTAL")
-	long grandTotal;
-	
+	Long grandTotal;
 	
 	@Column(name="CODE")
 	String code;
@@ -45,11 +44,8 @@ public class MarketPointsTotal {
 	String accountManager;
 	
 	
-	@Column(name="INCENTIVE")
-	Long incentive;
-	
 	@Id
-	@Column(name="Id")
-	long id;
+	@Column(name="ID")
+	Long id;
 
 }
