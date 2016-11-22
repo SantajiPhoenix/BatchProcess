5b758557206b7606287b2f39b3524b73c5156bbb
Bharat Tripathi <bharat.tripathi@yestelecom.nl>
2016-11-22 05:53:25 +0000

    Removed the commented lines

--- src/main/java/nl/yestelecom/phoenix/batch/BgProcessApplication.java
+++ src/main/java/nl/yestelecom/phoenix/batch/BgProcessApplication.java
@@ -4,15 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import nl.yestelecom.phoenix.batch.job.marketpoints.MarketPointsProcess;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 @EnableJpaAuditing
 @EnableSpringDataWebSupport
 @EnableTransactionManagement
+@PropertySource("file:/opt/phoenix/configuration/backgroudjobs.properties")
 public class BgProcessApplication {
 
 	public static void main(String[] args) {
