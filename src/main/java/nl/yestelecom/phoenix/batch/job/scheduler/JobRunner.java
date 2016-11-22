f7eb93692748f58c317b12b8550fd74d4e819f9e
Bharat Tripathi <bharat.tripathi@yestelecom.nl>
2016-11-22 07:00:43 +0000

    Added scheduling to all jobs

--- src/main/java/nl/yestelecom/phoenix/batch/job/scheduler/JobRunner.java
+++ src/main/java/nl/yestelecom/phoenix/batch/job/scheduler/JobRunner.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import nl.yestelecom.phoenix.batch.job.JobProcessor;
@@ -54,6 +55,7 @@ public class JobRunner {
 		return jobs;
 	}
 	
+	@Scheduled(cron="0 0 12 * * ? ")
 	public void runJobs(){
 		addJobs();
 		for(JobProcessor job : jobs){
