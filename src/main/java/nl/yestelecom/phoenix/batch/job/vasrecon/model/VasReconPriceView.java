efa92e00480f8f92322c8a218499b7b35767d26e
Bharat Tripathi <bharat.tripathi@yestelecom.nl>
2017-02-20 06:21:42 +0000

    Changed the name of the view

--- src/main/java/nl/yestelecom/phoenix/batch/job/vasrecon/model/VasReconPriceView.java
+++ src/main/java/nl/yestelecom/phoenix/batch/job/vasrecon/model/VasReconPriceView.java
@@ -10,45 +10,44 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "VAS_RECON_PRICE_VIEW")
+@Table(name = "V_VAS_RECON_PRICE_VIEW")
 @Data
 public class VasReconPriceView {
 
-	
-	@Id
-	@Column(name="GSS_ID")
-	Long gssId;
-	
-	@Column(name="SOURCE")
-	String source;
-	
-	@Column(name="CHARGE_CODE")
-	String chargeCode;
-	
-	@Column(name="SERVICE_CODE")
-	String ServiceCode;
-	
-	@Column(name="CODE")
-	String code;
-	
-	@Column(name="NAME")
-	String name;
-	
-	@Column(name="DATE_FROM")
-	Date dateFrom;
-	
-	@Column(name="BEGINDATE_VALID")
-	Date begindateValid;
-	
-	@Column(name="AMOUNT_BILLED")
-	Long amountBilled;
-	
-	@Column(name="SKT_PRICE")
-	Long sktPrice;
-	
-	@Column(name="PLANNED_AMOUNT")
-	Long plannedAMount;
-	
-	@Column(name="CONFIG_AMOUNT")
-	Long configAmount;
+    @Id
+    @Column(name = "GSS_ID")
+    Long gssId;
+
+    @Column(name = "SOURCE")
+    String source;
+
+    @Column(name = "CHARGE_CODE")
+    String chargeCode;
+
+    @Column(name = "SERVICE_CODE")
+    String ServiceCode;
+
+    @Column(name = "CODE")
+    String code;
+
+    @Column(name = "NAME")
+    String name;
+
+    @Column(name = "DATE_FROM")
+    Date dateFrom;
+
+    @Column(name = "BEGINDATE_VALID")
+    Date begindateValid;
+
+    @Column(name = "AMOUNT_BILLED")
+    Long amountBilled;
+
+    @Column(name = "SKT_PRICE")
+    Long sktPrice;
+
+    @Column(name = "PLANNED_AMOUNT")
+    Long plannedAMount;
+
+    @Column(name = "CONFIG_AMOUNT")
+    Long configAmount;
 }
--- src/main/java/nl/yestelecom/phoenix/batch/job/vasrecon/model/VasReconProductsView.java
+++ src/main/java/nl/yestelecom/phoenix/batch/job/vasrecon/model/VasReconProductsView.java
@@ -10,61 +10,59 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "VAS_RECON_DATA_VIEW")
+@Table(name = "V_VAS_RECON_DATA_VIEW")
 @Data
 public class VasReconProductsView {
-	@Id
-	@Column(name="GSS_ID")
-	Long gssId;
-	
-	@Column(name="GSS_PROD_ID")
-	Long gssProdId;
-	
-	@Column(name="C2Y_ID")
-	Long c2yId;
-	
-	@Column(name="SERVICE_CODE")
-	String serviceCode;
-	
-	@Column(name="START_DATE_CONTRACT")
-	Date startDateContract;
-	
-	@Column(name="START_DATE_CONTRACT_DATA")
-	Date startDateContractData;
-	
-	@Column(name="MODIFY_DATE")
-	Date modifyDate;
-	
-	@Column(name="ENTRY_DATE")
-	Date entryDate;
-	
-	@Column(name="BEGINDATE_VALID")
-	Date beginDateValid;
-	
-	@Column(name="END_DATE_VALID")
-	Date endDateValid;
-	
-	@Column(name="CODE")
-	String code;
-	
-	@Column(name="VAS_STATE_CODE")
-	String vasStateCode;
-	
-	@Column(name="VAS_CODE")
-	String vasCode;
-	
-	@Column(name="CES_ID")
-	Long cesId;
-	
-	@Column(name="PRODUCT_IN_ZYGO")
-	String productInZygo;
-	
-	@Column(name="PRODUCT_IN_SKT")
-	String productInSkt;
-	
-	@Column(name="SOURCE")
-	String source;
-	
-	
+    @Id
+    @Column(name = "GSS_ID")
+    Long gssId;
+
+    @Column(name = "GSS_PROD_ID")
+    Long gssProdId;
+
+    @Column(name = "C2Y_ID")
+    Long c2yId;
+
+    @Column(name = "SERVICE_CODE")
+    String serviceCode;
+
+    @Column(name = "START_DATE_CONTRACT")
+    Date startDateContract;
+
+    @Column(name = "START_DATE_CONTRACT_DATA")
+    Date startDateContractData;
+
+    @Column(name = "MODIFY_DATE")
+    Date modifyDate;
+
+    @Column(name = "ENTRY_DATE")
+    Date entryDate;
+
+    @Column(name = "BEGINDATE_VALID")
+    Date beginDateValid;
+
+    @Column(name = "END_DATE_VALID")
+    Date endDateValid;
+
+    @Column(name = "CODE")
+    String code;
+
+    @Column(name = "VAS_STATE_CODE")
+    String vasStateCode;
+
+    @Column(name = "VAS_CODE")
+    String vasCode;
+
+    @Column(name = "CES_ID")
+    Long cesId;
+
+    @Column(name = "PRODUCT_IN_ZYGO")
+    String productInZygo;
+
+    @Column(name = "PRODUCT_IN_SKT")
+    String productInSkt;
+
+    @Column(name = "SOURCE")
+    String source;
 
 }
