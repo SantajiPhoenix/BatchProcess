5074ea35a9dbf4f1a30b4a50d7f358d68382f136
Bharat Tripathi <bharat.tripathi@yestelecom.nl>
2017-02-22 07:29:20 +0000

    Sonar issue fixes

--- src/main/java/nl/yestelecom/phoenix/batch/BgProcessApplication.java
+++ src/main/java/nl/yestelecom/phoenix/batch/BgProcessApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import nl.yestelecom.phoenix.batch.job.marketpoints.MarketPointsProcess;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
@@ -20,8 +17,8 @@ import nl.yestelecom.phoenix.batch.job.marketpoints.MarketPointsProcess;
 @PropertySource("file:/opt/phoenix/configuration/backgroudjobs.properties")
 public class BgProcessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BgProcessApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BgProcessApplication.class, args);
+    }
 
 }
