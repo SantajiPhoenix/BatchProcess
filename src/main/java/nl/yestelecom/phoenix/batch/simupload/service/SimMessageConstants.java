ac37321b616ed16e0429e64f0d433734d8e007da
Sarvesh Kulkarni <sarvesh.kulkarni@yestelecom.nl>
2017-05-15 06:28:40 +0000

    pom update comment -  ENHANCEMENTS: Add PUK code to E-view Sprint: 21 Story Id: 76386 Reviewed by : sandeep and bharat
    Cockpit user story #76386 in the Sprint #%SPRNTID% for the epic %EPIC%

---
+++ src/main/java/nl/yestelecom/phoenix/batch/simupload/service/SimMessageConstants.java
@@ -0,0 +1,11 @@
+package nl.yestelecom.phoenix.batch.simupload.service;
+
+public class SimMessageConstants {
+
+    public static final String SIMLOADER = "SIMLOADER";
+    public static final String PUKLOADER = "PUKLOADER";
+    public static final String M2MSIMLOADER = "M2MSIMLOADER";
+
+    private SimMessageConstants() {
+    }
+}
