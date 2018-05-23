d14b4c63422ae8e42da12f3dd59cbcfd96c5418c
Santaji Suryawanshi <santaji.suryawanshi@yestelecom.nl>
2018-05-23 17:49:55 +0530

    [MASTER][#][Don't send empty emails for Porting requests]

--- src/main/java/nl/yestelecom/phoenix/batch/portingrequestreport/FailedPortingRequestProcessor.java
+++ src/main/java/nl/yestelecom/phoenix/batch/portingrequestreport/FailedPortingRequestProcessor.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,9 +67,11 @@ public class FailedPortingRequestProcessor implements JobProcessor {
     @Override
     public void send() {
         logger.info("SEND");
-        emailDetails.setText(mailText);
-        portingReportEmailSender.setEmailDetails(emailDetails);
-        portingReportEmailSender.accept(senderVisitor);
+        if (StringUtils.isNotEmpty(mailText)) {
+            emailDetails.setText(mailText);
+            portingReportEmailSender.setEmailDetails(emailDetails);
+            portingReportEmailSender.accept(senderVisitor);
+        }
 
     }
 
