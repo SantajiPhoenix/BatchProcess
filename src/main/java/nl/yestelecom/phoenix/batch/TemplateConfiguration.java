2961229deb887b6642f42483cdb296d63ae2e606
saakulkarni <saahas.kulkarni@yestelecom.nl>
2019-07-05 19:57:29 +0530

    [ENH][#][Fixed thymeleaf template config for Spring boot 2.1.2]

--- src/main/java/nl/yestelecom/phoenix/batch/TemplateConfiguration.java
+++ src/main/java/nl/yestelecom/phoenix/batch/TemplateConfiguration.java
@@ -1,24 +1,39 @@
 package nl.yestelecom.phoenix.batch;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.resourceresolver.ClassLoaderResourceResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
-public class TemplateConfiguration {
+public class TemplateConfiguration implements ApplicationContextAware {
 
-	@Bean(name = "classPathTemplateEngineXML")
-	public TemplateEngine templateEngineXML() {
-		TemplateResolver templateResolver = new TemplateResolver();
-		templateResolver.setPrefix("templates/");
-		templateResolver.setSuffix(".xml");
-		templateResolver.setResourceResolver(new ClassLoaderResourceResolver());
+    private ApplicationContext applicationContext;
 
-		TemplateEngine templateEngine = new TemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver);
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
-		return templateEngine;
-	}
+    @Bean(name = "classPathTemplateEngineXML")
+    public TemplateEngine templateEngineXML() {
+        final SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setEnableSpringELCompiler(true);
+        engine.setTemplateResolver(xmlTemplateResolver());
+        return engine;
+    }
+
+    private ITemplateResolver xmlTemplateResolver() {
+        final SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("classpath:templates/");
+        resolver.setSuffix(".xml");
+        resolver.setTemplateMode(TemplateMode.XML);
+        return resolver;
+    }
 }
